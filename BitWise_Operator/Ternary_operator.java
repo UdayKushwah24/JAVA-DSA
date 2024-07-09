b49a8ef5c64aeab3518057c67a6f3f886b3d3011
Uday kushwah <142593408+uday1234-5@users.noreply.github.com>
2024-07-09 12:11:54 +0530

    JAVA File

---
+++ BitWise_Operator/Ternary_operator.java
@@ -0,0 +1,8 @@
+package BitWise_Operator;
+
+public class Ternary_operator {
+    public static void main(String[] args) {
+        // String Ans = ((5%2)==0)?"Even":"Odd";
+        // System.out.println(Ans);
+    }
+}
